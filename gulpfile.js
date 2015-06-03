fe5027b798504c3f6f2ee5da6967ab76e27320bd
Alexander Nikitin <nikitindiz@gmail.com>
2015-06-04 01:56:43 +0700

    Env setup using gulp

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    gutil = require('gulp-util'),
+    compass = require('gulp-compass');
+
+// Watch Changes in Files
+gulp.task('watchFiles', function() {
+  gulp.watch('./assets/js/*.js', ['taskCoffee', 'reloadPageScripts']);
+  gulp.watch('./scss/*.scss', ['convertSass', 'reloadPage']);
+  gulp.watch('./*.html', ['reloadPageScripts']);
+});
+
+gulp.task('reloadPage', function() {
+  browserSync.reload({stream:true});
+});
+
+gulp.task('reloadPageScripts', function() {
+  browserSync.reload({stream:false});
+});
+
+// Browser Live View
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: "./",
+            index: "home.html"
+        }
+    });
+});
+
+gulp.task('convertSass', function() {
+  gulp.src('./scss/*.scss')
+    .pipe(compass({
+      config_file: './config.rb',
+      css: './assets/css',
+      sass: 'scss'
+    }))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('default', ['browserSync', 'convertSass', 'watchFiles']);
