366ec2e909be7480a2ed02c112bf1c11d5a9876d
Alexander Nikitin <nikitindiz@gmail.com>
2015-06-04 03:19:20 +0700

    Question button and question title now leads to single-question.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,23 @@
     window.location.href = 'search-result.html';
   };
   
+  $('.appBtnGoToQuestion')
+    .on('click', function(e){
+      window.location.href = 'single-question.html';
+    });
+  
+//  if (document.body.addEventListener) {
+//    document.body.addEventListener('click', goToQuestion,false);
+//  } else {
+//    document.body.attachEvent('onclick', goToQuestion);
+//  }
+//  
+//  function goToQuestion(e) {
+//    e = e || window.event;
+//    var target = e.target || e.srcElement;
+//    if (target.className.match(/appBtnGoToQuestion/)) {
+//      console.log('test');
+//    }
+//  }
+  
 })(window, jQuery);
